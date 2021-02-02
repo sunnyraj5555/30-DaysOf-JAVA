571e06e7ed1caa54ed6a90e056f287b13086fd4d
sunnyraj5555 <headheal2206@gmail.com>
2021-02-02 21:23:49 +0530

    Taking Input as Branch, Year and Semester

--- DAY2.java
+++ DAY2.java
@@ -4,4 +4,15 @@ public class DAY2
 {
 		public static void main(String[] args) 
 		{
-		
+		Scanner s = new Scanner(System.in);
+			
+			System.out.println("CSE-Computer Science || ME-Mechanical || CE-Chemical Engeniring");
+			System.out.println("Enter Your Branch from (CSE/ME/CE) : ");
+			String Branch = s.nextLine();
+			
+			System.out.println("Enter Your Year (in Roman) : ");
+			String Year = s.nextLine();
+			
+			System.out.println("Enter Your Semester (in Words) : ");
+			int Semester = s.nextInt();
+			
